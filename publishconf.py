e56d9c847077ac2f766a622cb2c5520e0dc1252a
Lucy Park <me@lucypark.kr>
2017-04-30 08:46:36 +0900

    Change http to https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://lucypark.kr/courses'
+SITEURL = 'https://lucypark.kr/courses'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
